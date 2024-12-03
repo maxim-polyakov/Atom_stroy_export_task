1584952d57e646e23130daa34b15f498f78ef602
maxim-polyakov <maxim7012@mail.ru>
2024-12-04 00:13:35 +0300

    Task

---
+++ Script.sql
@@ -0,0 +1,8 @@
+CREATE TABLE messtorage."План-факт анализ" (
+	объект varchar NULL,
+	"вид работ" varchar NULL,
+	дата varchar NULL,
+	сумма varchar NULL,
+	план varchar NULL,
+	"сумма факт" varchar NULL
+);
---
+++ schedule downloading data into db.py
@@ -0,0 +1,23 @@
+import psycopg2
+from sqlalchemy import create_engine
+import pandas as pd
+import datetime as dt
+import schedule
+
+
+conn_remote = psycopg2.connect('postgres://postgres:gaTResKPJX25@ep-round-paper-091468.us-east-2.aws.neon.tech/BotsMemory')
+engine_remote = create_engine(f'postgresql://postgres:gaTResKPJX25@ep-round-paper-091468.us-east-2.aws.neon.tech/BotsMemory')
+
+def insert_into_db():
+    print("importing data to dab")
+    df = pd.read_csv('Report.csv')
+    df.to_sql("План-факт анализ", con=engine_remote, schema='messtorage',
+          index=False, if_exists='append')
+
+schedule.every().seconds.do(insert_into_db)
+
+if __name__ == "__main__":
+#
+#
+    while True:
+        schedule.run_pending()
