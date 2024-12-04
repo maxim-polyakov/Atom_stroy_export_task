b5a7de3242a4ffe91ba28f6f4eecce70d7a8545e
maxim-polyakov <maxim7012@mail.ru>
2024-12-04 23:58:20 +0300

    Update view-1.sql

--- view-1.sql
+++ view-1.sql
@@ -1,1 +1,1 @@
-SELECT date_trunc('month', "date", ), sum("summ_plan") ,sum("summ_fact") FROM public."Plan-fact analysis" WHERE "object" = 1 GROUP BY date_trunc('month', "date")
+SELECT date_trunc('month', date), sum(summ_plan) ,sum(summ_fact) FROM public.plan_fact_analysis where object = 1 GROUP BY date_trunc('month', date)
